ec37825901b9edb0afbded6117a5fc48eb4a0651
RenaeSedluk <36517111+RenaeSedluk@users.noreply.github.com>
2018-06-12 14:13:19 -0500

    Create code.cpp
    Testing

---
+++ code.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+using namespace std;
+
+int hello = 152;
+int x = 34;
+int main ()
+{
+  string name = "Renae";
+  if (name != hello)
+  {
+    x = 0
+  }
+  cout << x << endl;
+}
